00e17e6591804f7953df205f01546989f72bf509
Lee Goolsbee (lee) <lee@edgecase.io>
2015-05-05 10:10:31 -0500

    Add module_root for use in templates

--- src/web.js
+++ src/web.js
@@ -19,6 +19,10 @@ module.exports = function (web) {
 		});
 	}));
 
+	app.locals = {
+		module_root : '/modules/facts'
+	};
+
 	app.use('/static', web.express.static(__dirname + '/../static/'));
 
 	web.addModuleApp('/facts/', app, 'Facts');

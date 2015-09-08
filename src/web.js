8a0f553e26aff390260c18b02a25312e07c54d06
Michael Pratt <urthen@gmail.com>
2015-09-08 17:34:45 -0500

    adding stupidly basic removal facilities

--- src/web.js
+++ src/web.js
@@ -9,15 +9,47 @@ module.exports = function (web) {
 
 	app.get('/', web.render(app, 'facts', function (req, next) {
 		web.bot.db.schemas.factFactoid.find({}).populate('trigger').execQ().then(function (facts) {
-			next({ facts : facts });
-		});
+			next({ facts : facts, admin : req.query.admin });
+		}).done();
 	}));
 
 	app.get('/words', web.render(app, 'words', function (req, next) {
 		web.bot.db.schemas.word.find({}).then(function (words) {
-			next({ words : words });
-		});
+			next({ words : words, admin : req.query.admin });
+		}).done();
 	}));
+
+	app.get('/remove', function (req, res) {
+
+		if (!req.query.id) {
+			web.error(res, 'Needs ID', "Don't mess around with things you don't understand.");
+			return;
+		}
+
+		var id = req.query.id;
+		console.log('Deleting fact:', id);
+
+		web.bot.db.schemas.factFactoid.remove({ _id : id })
+		.then(function () {
+			res.redirect('/modules/facts/');
+		}).done();
+	});
+
+	app.get('/remove/words', function (req, res) {
+
+		if (!req.query.id) {
+			web.error(res, 'Needs ID', "Don't mess around with things you don't understand.");
+			return;
+		}
+
+		var id = req.query.id;
+		console.log('Deleting word:', id);
+
+		web.bot.db.schemas.word.remove({ _id : id })
+		.then(function () {
+			res.redirect('/modules/facts/words');
+		}).done();
+	});
 
 	app.locals = {
 		module_root : '/modules/facts'
